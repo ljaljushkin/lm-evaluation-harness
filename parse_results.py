19b1cfa244abdcb89cf2855cde94ee6a43e98da5
Nikolay <nikolay.lyalyushkin@intel.com>
2023-09-06 10:45:39 +0200

    parse results

---
+++ parse_results.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+import json
+# s = Path('/home/nlyaly/projects/lm-evaluation-harness/runs')
+# s = Path('/home/devuser/nlyalyus/projects/lm-evaluation-harness/runs')
+
+paths = s.glob('**/results.json')
+paths = sorted(paths, key=os.path.getmtime)
+for path in paths:
+    print(path)
+    with path.open() as f:
+        j = json.load(f)
+        r = j['results']
+        print(json.dumps(r, indent=4))
+
