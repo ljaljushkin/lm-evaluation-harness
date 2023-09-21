3d52cb29872299e11d90a1afca677f2bf80c5a0e
Nikolay <nikolay.lyalyushkin@intel.com>
2023-09-21 15:02:43 +0200

    memory management + redirect output to file

--- main.py
+++ main.py
@@ -4,39 +4,94 @@ import logging
 import os
 
 from dataclasses import dataclass
-from lm_eval import tasks, evaluator, utils
+from lm_eval import evaluator
 import shutil
 import argparse
 from datetime import datetime
 import json
 import logging
-import fnmatch
 from pathlib import Path
 from time import time
 import random
 
 import torch
 from transformers import AutoModelForCausalLM
-from correct_ir import correct_attention_mask_names
 
-from lm_eval import tasks, evaluator
+from lm_eval import evaluator
 # from visualization import parse_results
-from openvino.runtime import serialize
-from openvino.tools.mo import convert_model
 
-from nncf import compress_weights
 import torch
-from functools import partial
 from transformers import AutoModelForCausalLM, AutoTokenizer
 # from memory_profiler import memory_usage
 from optimum.intel import OVModelForCausalLM
 
 from optimum.intel.openvino import OVConfig, OVQuantizer
-import datasets
-import nncf
 
 logging.getLogger("openai").setLevel(logging.WARNING)
 
+import openvino.runtime as ov
+from openvino import Core
+import time
+import queue
+import atexit
+from nncf import compress_weights
+from pathlib import Path
+import threading
+import matplotlib.pyplot as plt
+core = Core()
+
+
+import psutil
+
+memory_data_queue = queue.Queue()
+monitoring_thread_should_stop = False
+
+LOGS_DIR = Path("./logs_compress")
+
+
+def stop_monitoring_thread():
+    global monitoring_thread_should_stop
+    monitoring_thread_should_stop = True
+
+
+def monitor_memory(q):
+    while not monitoring_thread_should_stop:
+        memory_usage = psutil.Process().memory_info().rss >> 20     # MB
+        timestamp = datetime.now()
+        (datetime.now() - timestamp).total_seconds()
+        q.put((timestamp, memory_usage))
+        time.sleep(1)
+
+
+def log_memory_usage(log_dir):
+    memory_usage_data = []
+    while not memory_data_queue.empty():
+        timestamp, memory_usage = memory_data_queue.get()
+        memory_usage_data.append((timestamp, memory_usage))
+
+    # Save the memory usage data to a file
+    with open(log_dir / 'memory_usage_log.txt', 'w') as log_file:
+        for timestamp, memory_usage in memory_usage_data:
+            log_file.write(f"{timestamp} {memory_usage}\n")
+
+        log_file.writelines([
+            f"Total time: {(memory_usage_data[-1][0] - memory_usage_data[0][0]).total_seconds() // 60} (minutes)\n",
+            f"Max memory: {max(tuple(zip(*memory_usage_data))[1])} (MB)"])
+
+    timestamps, memory_usage = zip(*memory_usage_data)
+    plt.figure(figsize=(10, 6))
+    plt.plot(timestamps, memory_usage)
+    plt.xlabel("Time")
+    plt.ylabel("Memory Usage (MB)")
+    plt.title("Memory Usage vs. Time")
+    plt.grid(True)
+    plt.savefig(log_dir / "memory_usage.png")
+
+def start_memory_logging_routine(log_dir):
+    memory_monitor_thread = threading.Thread(target=monitor_memory, args=(memory_data_queue,))
+    memory_monitor_thread.daemon = True  # Daemonize the thread
+    memory_monitor_thread.start()
+    atexit.register(lambda: [stop_monitoring_thread(), memory_monitor_thread.join(), log_memory_usage(log_dir)])
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -188,8 +243,14 @@ def main():
         # ExpDesc('bigscience/bloom-7b1', group_size=-1, mode='pq'),
         # ExpDesc('meta-llama/Llama-2-7b-chat-hf', group_size=-1, mode='nf4', delete_ir_cache=True),
         # ExpDesc('meta-llama/Llama-2-7b-chat-hf', group_size=-1, mode='pq'),
-        # ExpDesc('meta-llama/Llama-2-13b-chat-hf', is_fp32=True),
-        ExpDesc('meta-llama/Llama-2-13b-chat-hf', do_eval=True, delete_ir_cache=False, exp_name='nf4_ov'),
+        # ExpDesc('meta-llama/Llama-2-7b-chat-hf', is_fp32=True, do_eval=False),
+        # ExpDesc('databricks/dolly-v2-3b', is_fp32=True, do_eval=False),
+
+        # ExpDesc('facebook/opt-125m', is_fp32=True),
+        # ExpDesc('databricks/dolly-v2-3b', do_eval=True, delete_ir_cache=False, exp_name='int8', limit=100),
+        # ExpDesc('meta-llama/Llama-2-13b-chat-hf', do_eval=True, delete_ir_cache=False, exp_name='nf4_ov'),
+        ExpDesc('meta-llama/Llama-2-13b-chat-hf', do_eval=True, delete_ir_cache=True, group_size=-1, mode='nf4'),
+        # ExpDesc('facebook/opt-125m', do_eval=True, delete_ir_cache=False, exp_name='nf4_ov_g128', limit=100),
     ]
     all_results_paths = []
     for desc in descs:
@@ -225,43 +286,53 @@ def main():
         os.symlink(ir_cache_dir.resolve(), log_dir.resolve() / ir_cache_dir.name)
         os.symlink(log_dir.resolve(), ir_cache_dir.resolve() / log_dir.name)
         time_dict = {}
-        if not ir_path.exists():
-            if 'fp32' not in encoded_name:
-                print(f'started weights compression')
-                start_time = time()
-                quantization_config = {
-                    "algorithm": "quantization"
-                }
-                model = AutoModelForCausalLM.from_pretrained(
-                    model_id, use_cache=use_pkv, trust_remote_code=True,
-                    # TODO: aidova tip to avoid issue with model.onnx and probably with compilation
-                    # torchscript=True,
-                    use_auth_token=True
-                )
-                print(model)
-                tokenizer = AutoTokenizer.from_pretrained(model_id)
+        log_file_path = log_dir / "log.txt"
 
-                config = OVConfig(compression=quantization_config)
-                config.target_device = "TRIAL"
-                tokenizer.pad_token = tokenizer.eos_token
+        with open(log_file_path, "w") as log_file:
+            sys.stdout = log_file
+            sys.stderr = log_file
+            log_file.flush()
 
-                quantizer = OVQuantizer.from_pretrained(model)
+            if not ir_path.exists():
+                if 'fp32' not in encoded_name:
+                    print(f'started weights compression')
+                    start_time = time()
+                    quantization_config = {
+                        "algorithm": "quantization"
+                    }
+                    model = AutoModelForCausalLM.from_pretrained(
+                        model_id, use_cache=use_pkv, trust_remote_code=True,
+                        # TODO: aidova tip to avoid issue with model.onnx and probably with compilation
+                        # torchscript=True,
+                        use_auth_token=True
+                    )
+                    print(model)
+                    tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-                if hasattr(model, "transformer") and hasattr(model.transformer, "wte") and type(model.transformer.wte) != torch.nn.Embedding:
-                    from nncf.torch import register_module
-                    register_module(ignored_algorithms=[], target_weight_dim_for_compression=1)(type(model.transformer.wte))
+                    config = OVConfig(compression=quantization_config)
+                    config.target_device = "TRIAL"
+                    tokenizer.pad_token = tokenizer.eos_token
 
-                quantizer.quantize(
-                    save_directory=ir_cache_dir, weights_only=True,
-                    group_size=desc.group_size, mode=desc.mode, is_mixed=desc.is_mixed
-                )
+                    quantizer = OVQuantizer.from_pretrained(model)
 
-                nncf_time = time() - start_time
-                time_dict['nncf'] = nncf_time
-                print(f'weights compression took {nncf_time} seconds')
-            else:
-                ov_model = OVModelForCausalLM.from_pretrained(model_id, use_cache=use_pkv, trust_remote_code=True, from_transformers=True)
-                ov_model.save_pretrained(ir_cache_dir)
+                    if hasattr(model, "transformer") and hasattr(model.transformer, "wte") and type(model.transformer.wte) != torch.nn.Embedding:
+                        from nncf.torch import register_module
+                        register_module(ignored_algorithms=[], target_weight_dim_for_compression=1)(type(model.transformer.wte))
+
+                    start_memory_logging_routine(log_dir)
+                    quantizer.quantize(
+                        save_directory=ir_cache_dir, weights_only=True,
+                        group_size=desc.group_size, mode=desc.mode, is_mixed=desc.is_mixed
+                    )
+
+                    nncf_time = time() - start_time
+                    time_dict['nncf'] = nncf_time
+                    print(f'weights compression took {nncf_time} seconds')
+                else:
+                    ov_model = OVModelForCausalLM.from_pretrained(model_id, use_cache=use_pkv, trust_remote_code=True, from_transformers=True)
+                    ov_model.save_pretrained(ir_cache_dir)
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
 
         model_args = f'pretrained={ir_cache_dir.resolve()}'
 
