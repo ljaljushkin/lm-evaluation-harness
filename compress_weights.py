4d8cd82c3bddae50b4f3e328f310dd50e43a583e
Nikolay <nikolay.lyalyushkin@intel.com>
2023-11-15 20:31:22 +0100

    compress with data, save logs to file, auto exp name

--- compress_weights.py
+++ compress_weights.py
@@ -1,207 +1,197 @@
-from dataclasses import dataclass
-from functools import partial
+import datetime
 import gc
 import shutil
-from typing import Callable
-import openvino.runtime as ov
-from openvino import Core
 import time
-import queue
-import atexit
-import datetime
-from nncf import compress_weights
-from pathlib import Path
-import threading
-import matplotlib.pyplot as plt
-from nncf.parameters import CompressWeightsMode
-from tqdm import tqdm
 import traceback
+from dataclasses import dataclass
+from functools import partial
+from pathlib import Path
+from typing import Callable
+
+import matplotlib.pyplot as plt
+import numpy as np
+import openvino.runtime as ov
+from datasets import load_dataset
+from openvino import Core
 from optimum.intel import OVModelForCausalLM
-from nncf import IgnoredScope
+from tqdm import tqdm
+from transformers import AutoTokenizer
+import os
+from contextlib import redirect_stdout, redirect_stderr
+
+from nncf import Dataset
+from nncf import compress_weights
+from nncf.parameters import CompressWeightsMode
 core = Core()
 
+def gen_pkv(num_heads, head_dim, num_layers=None):
+    if num_layers is None:
+        num_layers = num_heads
+    res = {}
+    for i in range(num_layers):
+        res[f"past_key_values.{i}.key"] = np.zeros((1, num_heads, 0, head_dim))
+        res[f"past_key_values.{i}.value"] = np.zeros((1, num_heads, 0, head_dim))
+    return res
 
-LOGS_DIR = Path("./logs_compress")
+def gen_pkv_bloom(num_heads, head_dim, num_layers=None):
+    if num_layers is None:
+        num_layers = num_heads
+    res = {}
+    for i in range(num_layers):
+        # key:  [batch_size, num_heads, head_dim, seq_length] -> [batch_size * num_heads, head_dim, seq_length]
+        # value: [batch_size, num_heads, seq_length, head_dim] -> [batch_size * num_heads, seq_length, head_dim]
+        res[f"past_key_values.{i}.key"] = np.zeros((1 * num_heads, head_dim, 0))
+        res[f"past_key_values.{i}.value"] = np.zeros((1 * num_heads, 0, head_dim))
+    return res
 
+def transform_func(item, tokenizer, gen_pkv_fn):
+    tokens = tokenizer(item['text'])
+    #return tokens['input_ids'], tokens['attention_mask']
+    attention_mask = np.expand_dims(np.array(tokens['attention_mask']), 0)
+    position_ids = attention_mask.cumsum(-1) - 1
+    position_ids = np.ma.array(position_ids, mask=attention_mask == 0)
+    position_ids.filled(fill_value=1)
+    res = {
+        'input_ids': np.expand_dims(np.array(tokens['input_ids']), 0),
+        'attention_mask': attention_mask,
+        # 'position_ids': position_ids
+    }
+    res.update(gen_pkv_fn())
+    return res
 
-
-# from optimum.intel import OVModelForCausalLM
-# MODEL_NAME = 'opt-125m'
-# use_pkv = True
-# ov_model = OVModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', use_cache=use_pkv, trust_remote_code=True, from_transformers=True)
-# ov_model.save_pretrained('/home/nlyaly/projects/nncf/tests/openvino')
-# ie = ov.Core()
+MODEL_IDS_VS_GEN_FN = {
+    'facebook/opt-125m': partial(gen_pkv, 12, 64),
+    'databricks/dolly-v2-3b': partial(gen_pkv, 32, 80),
+    'meta-llama/Llama-2-7b-chat-hf': partial(gen_pkv, 32, 128),
+    'facebook/opt-6.7b': partial(gen_pkv, 32, 128),
+    'bigscience/bloom-7b1': partial(gen_pkv_bloom, 32, 128, 30),
+    'togethercomputer/RedPajama-INCITE-7B-Instruct': partial(gen_pkv, 32, 128),
+    'meta-llama/Llama-2-13b-chat-hf': partial(gen_pkv, 40, 128),
+    'databricks/dolly-v2-12b': partial(gen_pkv, 40, 128, 36),
+    'openlm-research/open_llama_3b': None,
+    'THUDM/chatglm2-6b': None,
+    'THUDM/chatglm3-6b': None,
+    'HuggingFaceH4/zephyr-7b-beta': None,
+    'bigscience/bloomz-560m': None
+}
 
 @dataclass
 class ExpDesc:
     model_id: str
-    compress_fn: Callable
-    exp_name: str
-    is_bin_needed: bool = True
+    mode: CompressWeightsMode = CompressWeightsMode.INT4_SYM
+    ratio: int = 1
+    group_size: int = 128
+    is_revert: bool = False
+    is_data: bool = False
+
     def __str__(self):
-        return f'{self.model_id}___{self.exp_name}'
+        return f'{self.model_id} ----> {self.get_exp_name()}'
 
-int8_fn = compress_weights
-nf4_fn = partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=-1)
-nf4_g128_fn = partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=128)
-mixed_g128_fn = partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.5, group_size=128)
-nf4_g64_fn = partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=64)
-nf4_g32_fn = partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=32)
-nf4_g128_r80_fn = partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.8, group_size=128)
-int4_g128_nozp_r80_fn = partial(compress_weights, mode=CompressWeightsMode.INT4_SYM, ratio=0.8, group_size=128)
-int4_g128_nozp_fn = partial(compress_weights, mode=CompressWeightsMode.INT4_SYM, ratio=1, group_size=128)
-int4_g64_nozp_fn = partial(compress_weights, mode=CompressWeightsMode.INT4_SYM, ratio=1, group_size=64)
-int4_g128_fn = partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128)
+    def get_compress_fn(self):
+        nncf_dataset = None
+        if self.is_data:
+            gen_pkv_fn = MODEL_IDS_VS_GEN_FN[self.model_id]
+            tokenizer = AutoTokenizer.from_pretrained(self.model_id)
+            dataset = load_dataset('wikitext', 'wikitext-2-v1', split='train[:1000]')
+            dataset = dataset.filter(lambda example: len(example["text"]) > 128)
+            nncf_dataset = Dataset(dataset, partial(transform_func, tokenizer=tokenizer, gen_pkv_fn=gen_pkv_fn))
 
+        return partial(compress_weights, mode=self.mode, ratio=self.ratio, group_size=self.group_size, dataset=nncf_dataset, is_revert=self.is_revert)
 
-MODEL_IDS = [
-    # 'facebook/opt-125m',
-    'databricks/dolly-v2-3b',
-    # 'openlm-research/open_llama_3b',
-    'facebook/opt-6.7b',
-    'bigscience/bloom-7b1',
-    'togethercomputer/RedPajama-INCITE-7B-Instruct',
-    # 'databricks/dolly-v2-12b',
-    'meta-llama/Llama-2-7b-chat-hf',
-    'meta-llama/Llama-2-13b-chat-hf',
-    # 'chatglm2-6b',
-    # 'chatglm-6b',
-]
+    def get_exp_name(self):
+        result = "int4"
+        if self.mode == CompressWeightsMode.INT8:
+            result = "int8"
+        elif self.mode == CompressWeightsMode.NF4:
+            result = "nf4"
 
-MODES_AND_NAMES = [
-    # (nf4_g64_fn, 'nf4_ov_g64'),
-    # (nf4_g128_fn, 'nf4_ov_g128'),
-    # (int4_g128_fn, 'int4_g128'),
-    # (int4_g128_nozp_fn, 'int4_g128_nozp'),
-    # (int4_g64_nozp_fn, 'int4_g64_nozp'),
-    # (int4_g128_nozp_r80_fn, 'int4_g128_nozp_r80'),
-    # (nf4_g32_fn, 'nf4_ov_g32'),
-    # (nf4_fn, 'nf4_ov'),
-    (int8_fn, 'int8')
-]
+        if self.group_size != -1:
+            result += f'_g{self.group_size}'
+
+        if self.mode == CompressWeightsMode.INT4_SYM:
+            result += '_nozp'
+
+        if self.ratio != 1:
+            result += f'_r{self.ratio * 100:2.0f}'
+
+        if self.is_data:
+            if self.is_revert:
+                result += '_anti'
+            else:
+                result += '_criteria'
+        else:
+            if self.is_revert:
+                result += '_a0'
+        return result
+
 
 
 EXP_DESCS= [
-#     # ExpDesc('meta-llama/Llama-2-13b-chat-hf', mixed_g128_fn, 'mixed_ov_g128'),
-#     # ExpDesc('meta-llama/Llama-2-7b-chat-hf', mixed_g128_fn, 'mixed_ov_g128'),
-#     # ExpDesc('meta-llama/Llama-2-13b-chat-hf', nf4_g128_fn, 'nf4_ov_g128', is_bin_needed=True),
-#     # ExpDesc('meta-llama/Llama-2-7b-chat-hf', nf4_g128_fn, 'nf4_ov_g128', is_bin_needed=True),
-#     ExpDesc('meta-llama/Llama-2-7b-chat-hf', nf4_g64_fn, 'nf4_ov_g64', is_bin_needed=True),
-#     # ExpDesc('meta-llama/Llama-2-13b-chat-hf', nf4_g64_fn, 'nf4_g64_ov', is_bin_needed=True),
-#     # ExpDesc('togethercomputer/RedPajama-INCITE-7B-Instruct', nf4_g64_fn, 'nf4_ov_g64', is_bin_needed=True),
-#     # ExpDesc('chatglm2-6b', int8_fn, 'int8', is_bin_needed=True),
-#     # ExpDesc('chatglm2-6b', nf4_fn, 'nf4_ov', is_bin_needed=True),
-    # ExpDesc('bigscience/bloom-7b1', nf4_fn, 'nf4_ov', is_bin_needed=True),
-    #     # ExpDesc('chatglm2-6b', nf4_g64_fn, 'nf4_ov_g64', is_bin_needed=True),
-#     #ExpDesc('chatglm2-6b', nf4_g32_fn, 'nf4_ov_g32', is_bin_needed=True),
-#     ExpDesc('dolly-v2-12b', nf4_g64_fn, 'nf4_ov_g64', is_bin_needed=True),
-#     # ExpDesc('databricks/dolly-v2-3b', nf4_g64_fn, 'nf4_ov_g64', is_bin_needed=True),
-#     # CLX
+    # ExpDesc('facebook/opt-125m', mode=CompressWeightsMode.NF4, ratio=0.5, group_size=64),
+    ExpDesc('bigscience/bloomz-560m', mode=CompressWeightsMode.INT4_SYM),
+    ExpDesc('bigscience/bloomz-560m', mode=CompressWeightsMode.INT4_ASYM, ratio=0.8, group_size=128),
+    # ExpDesc('facebook/opt-125m', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128),
+    # ExpDesc('facebook/opt-125m', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128, is_revert=True),
+    # ExpDesc('facebook/opt-125m', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128, is_data=True),
+    # ExpDesc('facebook/opt-125m', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128, is_data=True, is_revert=True),
 
-    # ExpDesc('bigscience/bloom-7b1', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.8, group_size=32), 'nf4_ov_g32_r80'),
-    # ExpDesc('bigscience/bloom-7b1', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.6, group_size=64), 'nf4_ov_g64_r60'),
-    # ExpDesc('bigscience/bloom-7b1', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.6, group_size=128), 'nf4_ov_g128_r60'),
-    # ExpDesc('bigscience/bloom-7b1', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.8, group_size=32), 'int4_ov_g32_r80'),
-    # ExpDesc('bigscience/bloom-7b1', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.6, group_size=64), 'int4_ov_g64_r60'),
-    # ExpDesc('bigscience/bloom-7b1', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.6, group_size=128), 'int4_ov_g128_r60'),
-
-    # ExpDesc('togethercomputer/RedPajama-INCITE-7B-Instruct', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.8, group_size=32), 'nf4_ov_g32_r80'),
-    # ExpDesc('togethercomputer/RedPajama-INCITE-7B-Instruct', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.6, group_size=32), 'nf4_ov_g32_r60'),
-
-
-    # ExpDesc('databricks/dolly-v2-3b', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.8, group_size=128), 'nf4_ov_g128_r80'),
-    # ExpDesc('databricks/dolly-v2-3b', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=0.6, group_size=128), 'nf4_ov_g128_r60'),
-
-    # ExpDesc('meta-llama/Llama-2-7b-chat-hf', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=128), 'nf4_ov_g128'),
-    # ExpDesc('meta-llama/Llama-2-13b-chat-hf', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=128), 'nf4_ov_g128'),
-
-    # ExpDesc('facebook/opt-6.7b', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=128), 'nf4_ov_g128'),
-    # ExpDesc('facebook/opt-6.7b', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=64), 'int4_ov_g64'),
-    # ExpDesc('facebook/opt-6.7b', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.8, group_size=64), 'int4_ov_g64_r80'),
-    # ExpDesc('facebook/opt-6.7b', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.6, group_size=64), 'int4_ov_g64_r60'),
-    # ExpDesc('facebook/opt-6.7b', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=32), 'int4_ov_g32'),
-    # ExpDesc('facebook/opt-6.7b', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.8, group_size=32), 'int4_ov_g32_r80'),
-    # ExpDesc('facebook/opt-6.7b', partial(compress_weights, mode=CompressWeightsMode.INT4_SYM, ratio=1, group_size=128), 'int4_g128'),
-    # ExpDesc('databricks/dolly-v2-3b', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.5, group_size=32), 'int4_ov_g32_r50'),
-    # ExpDesc('databricks/dolly-v2-3b', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.4, group_size=64), 'int4_ov_g64_r40'),
-
-    # ExpDesc('togethercomputer/RedPajama-INCITE-7B-Instruct', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128), 'int4_ov_g128'),
-    # ExpDesc('togethercomputer/RedPajama-INCITE-7B-Instruct', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.8, group_size=128), 'int4_ov_g128_r80'),
-
-    # ExpDesc('meta-llama/Llama-2-13b-chat-hf', partial(compress_weights, mode=CompressWeightsMode.INT4_SYM, ratio=0.8, group_size=64), 'int4_ov_g64_nozp_r80'),
-
-#     # ExpDesc('open_llama_3b', nf4_g128_fn, 'nf4_ov_g128', is_bin_needed=True),
-#     # ExpDesc('open_llama_3b', nf4_fn, 'nf4_ov', is_bin_needed=True),
-    ExpDesc('HuggingFaceH4/zephyr-7b-beta', partial(compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=128), 'nf4_g128'),
-    ExpDesc('HuggingFaceH4/zephyr-7b-beta', partial(compress_weights, mode=CompressWeightsMode.INT8), 'int8'),
-    ExpDesc('HuggingFaceH4/zephyr-7b-beta', partial(compress_weights, mode=CompressWeightsMode.INT4_ASYM, ratio=0.8, group_size=128), 'int4_g128_nozp_r80'),
-    # ExpDesc('THUDM/chatglm3-6b',
-    #     partial(
-    #         compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=128,
-    #         ignored_scope=IgnoredScope(["__module.transformer.embedding.word_embeddings/aten::embedding/Gather", "__module.transformer/aten::index_67/Gather"])
-    #     ),
-    #     'nf4_g128'
-    # ),
-    # ExpDesc('THUDM/chatglm3-6b', partial(compress_weights, mode=CompressWeightsMode.INT8), 'int8'),
-    # ExpDesc('THUDM/chatglm2-6b',
-    #     partial(
-    #         compress_weights, mode=CompressWeightsMode.NF4, ratio=1, group_size=128,
-    #         ignored_scope=IgnoredScope(["__module.transformer.embedding.word_embeddings/aten::embedding/Gather", "__module.transformer/aten::index_67/Gather"])
-    #     ),
-    #     'nf4_g128'
-    # ),
-    # ExpDesc('THUDM/chatglm2-6b', partial(compress_weights, mode=CompressWeightsMode.INT8), 'int8'),
+    # ExpDesc('databricks/dolly-v2-3b', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128),
+    # ExpDesc('databricks/dolly-v2-3b', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128, is_revert=True)),
+    # ExpDesc('databricks/dolly-v2-3b', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128, is_data=True),
+    # ExpDesc('databricks/dolly-v2-3b', mode=CompressWeightsMode.INT4_ASYM, ratio=1, group_size=128, is_data=True, is_revert=True),
 ]
 
 # EXP_DESCS = [ExpDesc(model_id, fn, name) for model_id in MODEL_IDS for fn, name in MODES_AND_NAMES]
-
-
 
 is_bin_needed = True
 cache_dir = Path('cache')
 ov_name = 'openvino_model.xml'
 
-# for model_id in MODEL_IDS:
-#     for compress_fn, exp_name in MODES_AND_NAMES:
+print('All experiments summary:')
+for desc in EXP_DESCS:
+    print(desc)
+
 for desc in tqdm(EXP_DESCS):
     model_id = desc.model_id
-    compress_fn = desc.compress_fn
-    exp_name = desc.exp_name
+    exp_name = desc.get_exp_name()
     model_name = Path(model_id).name
     SRC_PATH = cache_dir / model_name / 'fp32'/  ov_name
-    print(SRC_PATH)
-    try:
-        if not SRC_PATH.with_suffix('.bin').exists():
-            use_pkv = True
-            ov_model = OVModelForCausalLM.from_pretrained(model_id, use_cache=use_pkv, trust_remote_code=True, export=True)
-            ov_model.save_pretrained(SRC_PATH.parent)
-            ov_model._save_config(SRC_PATH.parent)
-            fp32_model = ov_model.model
-        else:
-            fp32_model = core.read_model(model=SRC_PATH)
-    except Exception as error:
-        print("Reading FP32 model failed:", error)
-        continue
-
-    print(f'\n\n{model_id}___{exp_name}')
-
     DST_PATH = cache_dir / model_name / exp_name /  ov_name
     DST_PATH.parent.mkdir(exist_ok=True)
-    print(DST_PATH)
-    shutil.copyfile(SRC_PATH.parent / 'config.json', DST_PATH.parent / 'config.json')
 
-    try:
+    with open(DST_PATH / 'output.txt', 'w') as f, redirect_stdout(f), redirect_stderr(f):
+        print(desc)
+        print(SRC_PATH)
+        print(DST_PATH)
+        try:
+            if not SRC_PATH.with_suffix('.bin').exists():
+                use_pkv = True
+                ov_model = OVModelForCausalLM.from_pretrained(model_id, use_cache=use_pkv, trust_remote_code=True, export=True)
+                ov_model.save_pretrained(SRC_PATH.parent)
+                ov_model._save_config(SRC_PATH.parent)
+                fp32_model = ov_model.model
+            else:
+                fp32_model = core.read_model(model=SRC_PATH)
+        except Exception as error:
+            print("Reading FP32 model failed:", error)
+            continue
+        shutil.copyfile(SRC_PATH.parent / 'config.json', DST_PATH.parent / 'config.json')
+
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        tokenizer.save_pretrained(DST_PATH.parent)
+
+        try:
+            start = time.time()
+            model = desc.get_compress_fn()(fp32_model)
+            print(f'compressing weights took {(time.time() - start):.1f} seconds')
+        except Exception as error:
+            print("Compression failed:", error)
+            print(traceback.print_exc())
+            continue
+
         start = time.time()
-        model = compress_fn(fp32_model)
-        print(f'compressing weights took {(time.time() - start):.1f} seconds')
-    except Exception as error:
-        print("Compression failed:", error)
-        print(traceback.print_exc())
-        continue
-
-    start = time.time()
-    ov.save_model(model, DST_PATH, compress_to_fp16=False)
-    print(f"saving model {DST_PATH} took {(time.time() - start):.1f} seconds")
+        ov.save_model(model, DST_PATH, compress_to_fp16=False)
+        print(f"saving model {DST_PATH} took {(time.time() - start):.1f} seconds")
 
     if not is_bin_needed:
         file_to_remove = DST_PATH.rename(DST_PATH.with_suffix('.bin'))
